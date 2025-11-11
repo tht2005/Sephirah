95731bc0884e0345ffdde47412978885e8c8ceb6
Nguyen Dinh Dang Duong <dangduong31205@gmail.com>
2025-11-11 16:26:55 +0700

    Update uci.cpp

--- src/uci.cpp
+++ src/uci.cpp
@@ -1,88 +1,80 @@
 #include "uci.h"
 #include "option.h"
+#include "position.h"
 #include "sephirah.h"
 #include <algorithm>
 #include <cctype>
-#include <functional>
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <variant>
-#include <vector>
 
 namespace UCI {
 
+void uci() {
+	std::cout << "id name " SEPHIRAH_NAME " " SEPHIRAH_VERSION << std::endl;
+	std::cout << "id author " SEPHIRAH_AUTHOR << std::endl;
+	std::cout << std::endl;
+	for (const auto& [_, op] : Options) {
+		std::cout << op << std::endl;
+	}
+	std::cout << "uciok" << std::endl;
+}
+
+void setoption(std::istringstream& ss, std::string& token) {
+	std::string name;
+	ss >> token >> name;
+	while (ss >> token && token != "value")
+		name = name + " " + token;
+	if (Options.count(name) == 0) {
+		std::cout << "Invalid option '" << name << "'" << std::endl;
+		return;
+	}
+	Option& op = Options[name];
+	if (op.type != "button") {
+		ss >> token;
+		if (op.type == "spin") {
+			int value;
+			ss >> value;
+			if (op.min <= value && value <= op.max) {
+				op.value = value;
+			}
+		} else {
+			ss >> token;
+			// convert to lowercase
+			std::transform(token.cbegin(), token.cend(), token.begin(),
+				[](unsigned char c){ return std::tolower(c); });
+			op.value = token;
+		}
+	}
+	if (op.on_change)
+		op.on_change(op);
+}
+
 int main(int argc, char **argv) {
-	Option::init();
+	Position pos;
+	// StateListPtr stlist;
+
 	std::cout << SEPHIRAH_NAME " " SEPHIRAH_VERSION " by " SEPHIRAH_AUTHOR << std::endl;
 	while (1) {
 		std::string cmd;
 		std::getline(std::cin, cmd);
 		std::istringstream ss(cmd);
-		std::string s;
-		std::vector<std::string> tokens;
-		while (ss >> s) tokens.push_back(s);
-		if (tokens.empty()) continue;
+		std::string token;
 
-		if (tokens[0] == "uci") {
-			std::cout << "id name " SEPHIRAH_NAME " " SEPHIRAH_VERSION << std::endl;
-			std::cout << "id author " SEPHIRAH_AUTHOR << std::endl;
-			std::cout << std::endl;
-			for (const auto& [_, op] : Options) {
-				std::cout << op << std::endl;
-			}
-			std::cout << "uciok" << std::endl;
-		} else if (tokens[0] == "isready") {
-			std::cout << "readyok" << std::endl;
-		} else if (tokens[0] == "setoption") {
-			if (tokens.size() <= 2 || tokens[1] != "name") {
-				std::cout << "Invalid setoption syntax" << std::endl;
-				continue;
-			}
-			int i = 3;
-			std::string name = tokens[2];
-			while (i < (int)tokens.size() && tokens[i] != "value")
-				name = name + " " + tokens[i++];
-			if (Options.count(name) == 0) {
-				std::cout << "Invalid option '" << name << "'" << std::endl;
-				continue;
-			}
-			Option& op = Options[name];
-			if (op.type != "button") {
-				if (i + 1 >= (int)tokens.size()) {
-					std::cout << "Need to specify value" << std::endl;
-					continue;
-				}
-				std::string valuestr = tokens[i + 1];
-				if (op.type == "spin") {
-					int value = std::stoi(valuestr);
-					if (op.min <= value && value <= op.max) {
-						op.value = value;
-					}
-				} else {
-					// convert to lowercase
-					std::transform(valuestr.cbegin(), valuestr.cend(), valuestr.begin(),
-						[](unsigned char c){ return std::tolower(c); });
-					op.value = valuestr;
-				}
-			}
-			if (op.on_change)
-				op.on_change(op);
-		} else if (tokens[0] == "ucinewgame") {
-			//TODO
-		} else if (tokens[0] == "position") {
-			//TODO
-		} else if (tokens[0] == "go") {
-			// report best move? update state
-		} else if (tokens[0] == "stop") {
-
-		} else if (tokens[0] == "ponderhit") {
-
-		} else if (tokens[0] == "quit") {
-			break;
-		} else {
-			std::cout << "No such command '" << tokens[0] << "'." << std::endl;
+		if (!(ss >> std::skipws >> token)) {
+			continue;
 		}
+
+		if (token == "uci") uci();
+		else if (token == "isready") std::cout << "readyok" << std::endl;
+		else if (token == "setoption") setoption(ss, token);
+		else if (token == "ucinewgame") ;
+		else if (token == "position") ;
+		else if (token == "go") ;
+		else if (token == "stop") ;
+		else if (token == "ponderhit") ;
+		else if (token == "quit") break;
+		else  std::cout << "No such command '" << token << "'" << std::endl;
 	}
 	return 0;
 }
